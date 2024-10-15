4a095c0e8da1f408b75c3ae618948298a043041c
zainabbasi38 <zainabbsi7534@gmail.com>
2024-10-15 02:46:17 -0700

    Add New File

---
+++ calculator .py
@@ -0,0 +1,20 @@
+a = int(input("Enter number:"))
+b = int(input("Enter number:"))
+
+operation = input("Please enter an operator (+,-,*,/):" )
+
+if operation == "+":
+    total = a + b
+    print(f"total: {total}")
+
+elif operation == "-":
+    total = a - b
+    print(f"total: {total}")
+
+elif operation == "*":
+    total = a * b
+    print(f"total: {total}")
+
+else:
+    total = a / b 
+    pritn(f"total: {total}")
